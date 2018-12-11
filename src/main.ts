c15a01c3175c47a79c246a8687f6fdfcb73c32e9
Mavrick Laasko <laakso.mavrick@gmail.com>
2018-12-11 16:15:06 -0500

    DI choices

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,40 @@
 import express from "express";
-const app = express();
+let app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("hello, world"));
+interface Server {
+  db: any;
+}
+
+const server: Server = { db: "db" };
+
+const healthz = (server: Server) => (
+  req: express.Request,
+  res: express.Response,
+): void => {
+  res.send({
+    server: true,
+    db: server.db === "db"
+  });
+};
+
+app.get("/healthz", healthz(server));
+
+// or
+
+const wire = (app: express.Express): express.Express => {
+  app.locals.db = "db";
+  return app;
+};
+
+app = wire(app);
+
+app.get("/healthz2", (req, res) => {
+  const { db } = req.app.locals;
+  res.send({
+    server: true,
+    db: db === "db"
+  });
+});
 
 app.listen(port, () => console.log(`app listening port:${port}`));
